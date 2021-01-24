46c4a9c030ea392a9cc2d0503aa8540f22435cb9
Sean Harris <seanharris@Seans-MacBook-Pro.local>
2021-01-24 22:09:57 +0000

    fix second build error on email thanks page

--- web/src/pages/thankyou.js
+++ web/src/pages/thankyou.js
@@ -8,7 +8,7 @@ import { responsiveTitle1 } from '../components/typography.module.css'
 const ThankYouPage = () => {
   return (
     <Layout>
-      <SEO title={thankyou} />
+      <SEO title="Thanks" />
       <Container>
         <h1 className={responsiveTitle1}>Thanks :) I'll be in touch 📱</h1>
         <p>Thank you!</p>
