37cdaa1f84292c8800ae52135de85a507ccbc75d
Sean Harris <seanharris@Seans-MacBook-Pro.local>
2021-02-04 21:55:39 +0000

    remove unneeded styles

--- web/src/styles/ContactStyles.js
+++ web/src/styles/ContactStyles.js
@@ -26,12 +26,6 @@ const ContactStyles = styled.form`
     label + label {
       margin-top: 1rem;
     }
-    &.order,
-    &.menu {
-      grid-column: span 1;
-      /* Chrome is weird about Grid and fieldsets, so we add a fixed height to fix it :)  */
-      height: 600px;
-    }
   }
   .mapleSyrup {
     display: none;
